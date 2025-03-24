07caf99b6f93a08f3cea3aaa9cfc7e0e455e8c0b
Osmar Domingos <osdomingos@gmail.com>
2025-03-24 09:42:05 -0300

    5 try

--- index.js
+++ index.js
@@ -22,6 +22,8 @@ class User {
   constructor(username) {
     this.username = String(username);
     this._id = uuidv4();
+    this.exercise;
+
   }
 };
 
@@ -36,7 +38,30 @@ app.post('/api/users', (req, res) => {
 
 // Ver uma lista de usuários
 app.get('/api/users', (req, res) => {
-  res.json(listaDeUsers)
+  res.json(listaDeUsers);
+});
+
+// Postar exercícios
+app.post('/api/users/:id/exercises' , (req, res) => {
+  let id = String(req.params.id);
+  console.log(id);
+  let usuario;
+  let date = !req.body.date ? new Date().toDateString() : req.body.date.toDateString();
+  console.log(date);
+  console.log(listaDeUsers[0]._id);
+  for (let i = 0; i < listaDeUsers.length; i++) {
+    console.log(listaDeUsers[i]);
+    if (listaDeUsers[i]._id == id) return usuario = listaDeUsers[i]
+  }
+  console.log(usuario);
+  usuario.exercise = {
+    username: usuario.username,
+    description: String(req.body.description),
+    duration: Number(req.body.duration),
+    date: date
+  };
+  console.log(usuario.exercise);
+  res.json(usuario);
 });
 
 
